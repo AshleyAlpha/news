f54b8920b99de362f06fdfa15f943861ca7e657c
Wecode <mashleyalpha@gmail.com>
2019-02-18 16:18:44 +0200

    creating an api key

---
+++ app/models/article.py
@@ -0,0 +1,13 @@
+class Article:
+    '''
+   Article class to define Article Objects
+    '''
+
+    def __init__(self,id,name,author,title,description,url,content):
+        self.id =id
+        self.name = name
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self. content = content
---
+++ app/models/source.py
@@ -0,0 +1,12 @@
+class Source:
+    '''
+    Source class to define Source Objects
+    '''
+
+    def __init__(self,id,name,description,language,country,category):
+        self.id =id
+        self.name = name
+        self.description = description
+        self.language = language
+        self.country = country
+        self.category = category
